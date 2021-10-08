30ba5b9620e24417ad831a18be6a68c39c1a74ae
schvv31n <tim.kurdov@gmail.com>
2021-10-08 11:21:12 +0300

    bugfix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 import os
 import multiprocessing
 
-bind = ":".join([os.environ["HOST_URL"], os.emviron["PORT"]])
+bind = ":".join([os.environ["HOST_URL"], os.environ["PORT"]])
 workers = multiprocessing.cpu_count() * 2 + 1
